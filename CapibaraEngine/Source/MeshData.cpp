4bc1fc1be7220d3be25523c101df266d664020b3
Hydeon-git <61042411+Hydeon-git@users.noreply.github.com>
2021-10-19 22:56:07 +0200

    GL_ARRAY -> GL_ELEMENT_ARRAY

--- CapibaraEngine/Source/MeshData.cpp
+++ CapibaraEngine/Source/MeshData.cpp
@@ -13,13 +13,13 @@ void MeshData::CreateMeshBuffers()
 	// Initialization of the vertex and index from the mesh data
 	// Vertex
 	glGenBuffers(1, &id_vertex);
-	glBindBuffer(GL_ARRAY_BUFFER, id_vertex);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * num_vertex * 3, vertex, GL_STATIC_DRAW);	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id_vertex);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * num_vertex * 3, vertex, GL_STATIC_DRAW);	
 
 	// Index
 	glGenBuffers(1, &id_index);
-	glBindBuffer(GL_ARRAY_BUFFER, id_index);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(uint) * num_index, index, GL_STATIC_DRAW);	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id_index);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * num_index, index, GL_STATIC_DRAW);	
 }
 
 void MeshData::DrawMesh()
